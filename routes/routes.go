b319bf1f7382f2f34f1ff77f34f807620fa921e7
Morris Morrison <mwlltrdev@gmail.com>
2025-01-01 22:33:57 +0100

    change package name handlers to api

--- routes/routes.go
+++ routes/routes.go
@@ -1,21 +1,21 @@
 package routes
 
 import (
-	"ytclipper-go/handlers"
+	"ytclipper-go/api"
 
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterRoutes(e *echo.Echo) {
-    e.GET("/", handlers.RenderHomePage)
+    e.GET("/", api.RenderHomePage)
     e.GET("/health", func(c echo.Context) error {
         return c.String(200, "Server is running")
     })
 
-    e.POST("/api/v1/clip", handlers.CreateClip)
-    e.GET("/api/v1/clip", handlers.GetClip)
-    e.GET("/api/v1/jobs/status", handlers.GetJobStatus)
+    e.POST("/api/v1/clip", api.CreateClip)
+    e.GET("/api/v1/clip", api.GetClip)
+    e.GET("/api/v1/jobs/status", api.GetJobStatus)
 
-    e.GET("/api/v1/video/duration", handlers.GetVideoDuration)
-    e.GET("/api/v1/video/formats", handlers.GetAvailableFormats)
+    e.GET("/api/v1/video/duration", api.GetVideoDuration)
+    e.GET("/api/v1/video/formats", api.GetAvailableFormats)
 }
