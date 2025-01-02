87fd39b52f27a87d011963fd245950a7bba12305
Morris Morrison <mwlltrdev@gmail.com>
2025-01-02 13:51:44 +0100

    change order of tests

--- test/e2e.go
+++ test/e2e.go
@@ -42,12 +42,12 @@ type Test struct {
 func main() {
 	tests := []Test{
 		{
-			Name: "Invalid YouTube URL Test",
-			Run:  testInvalidYouTubeURL,
-		},
-		{
 			Name: "Invalid Timestamps Test",
 			Run:  testInvalidTimestamps,
+		},
+		{
+			Name: "Invalid YouTube URL Test",
+			Run:  testInvalidYouTubeURL,
 		},
 		{
 			Name: "Dark Mode Test",
